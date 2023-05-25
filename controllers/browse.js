9842b6125f96a4725eb68d0fe0acd9b257c3b20f
LeifCastle <leifurcastle@gmail.com>
2023-05-24 22:38:04 -0700

    feat: basic search functionality getting movie data from TMDB API on /browse

--- controllers/browse.js
+++ controllers/browse.js
@@ -3,7 +3,23 @@ const router = express.Router();
 const axios = require("axios");
 
 router.get("/", function (req, res) {
-  return res.render("browse/browse");
+  return res.render("browse/browse", { searchResult: null });
+});
+
+router.post("/", function (req, res) {
+  let base = "https://api.themoviedb.org";
+  let query = req.body.paramater;
+  let append = "append_to_response=images"; /*,videos as well*/
+  console.log(query);
+  axios
+    .get(
+      `${base}/3/search/movie?query=${query}&api_key=f95c55e2a9a5625ec1d3b5c42e143840&${append}`
+    )
+    .then((response) => {
+      //console.log(response.data.results); //response.data.results is an array of objects
+      res.render("browse/browse", { searchResult: response.data.results });
+    })
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
